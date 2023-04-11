566c0419f9f6fa7c9d48f11e53297f572326c133
Nicholas Stephan <nicholas.stephan@gmail.com>
2023-04-11 14:13:12 -0700

    wip

--- forge.config.js
+++ forge.config.js
@@ -7,6 +7,12 @@ module.exports = {
     {
       name: '@electron-forge/maker-appx',
       config: {
+        packageName: "WTQ",
+        packageDisplayName: "What the Quote",
+        packageDescription: "What the Quote do you want",
+        packageVersion: "1.0.0.0",
+        deploy: false,
+        publisherDisplayName: "Wonderful Terrific Fantastic",
         publisher: 'CN=F0D59367-F8E9-4DE8-865A-2D183D96ACC7',
       }
     }
