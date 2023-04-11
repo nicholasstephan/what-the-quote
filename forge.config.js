cd84ad645488335110907c33bc871eede7efa5ad
Nicholas Stephan <nicholas.stephan@gmail.com>
2023-04-11 14:21:40 -0700

    wip

--- forge.config.js
+++ forge.config.js
@@ -12,6 +12,7 @@ module.exports = {
         packageDescription: "What the Quote do you want",
         packageVersion: "1.0.0.0",
         deploy: false,
+        identityName: "7052WonderfulTerrificFant.WhattheQuote",
         publisherDisplayName: "Wonderful Terrific Fantastic",
         publisher: 'CN=F0D59367-F8E9-4DE8-865A-2D183D96ACC7',
       }
