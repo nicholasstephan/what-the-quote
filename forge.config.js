69da415359b2cb0e77db05d582eaf71119eeccd7
Nicholas Stephan <nicholas.stephan@gmail.com>
2023-04-11 12:23:43 -0700

    publisher id

--- forge.config.js
+++ forge.config.js
@@ -7,7 +7,7 @@ module.exports = {
     {
       name: '@electron-forge/maker-appx',
       config: {
-        publisher: 'WTF',
+        publisher: 'CN=F0D59367-F8E9-4DE8-865A-2D183D96ACC7',
       }
     }
   ],
